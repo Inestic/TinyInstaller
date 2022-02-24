d5afa47960c69e97514eaf66e9c8b51e34f96217
Inestic <50652175+Inestic@users.noreply.github.com>
2022-02-24 17:12:27 +0300

    Added spinner animation

---
+++ TinyInstaller/TinyInstaller/UserControls/Spinner.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace TinyInstaller.UserControls
+{
+    /// <summary>
+    /// Логика взаимодействия для Spinner.xaml
+    /// </summary>
+    public partial class Spinner : UserControl
+    {
+        public Spinner()
+        {
+            InitializeComponent();
+        }
+    }
+}
