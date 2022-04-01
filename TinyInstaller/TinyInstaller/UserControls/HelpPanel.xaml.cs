de81de8a5d0a37387559f7cab3fb8b05acc03014
Inestic <50652175+Inestic@users.noreply.github.com>
2022-04-01 15:47:16 +0300

    Added "Create config file" command :zap:

--- TinyInstaller/TinyInstaller/UserControls/HelpPanel.xaml.cs
+++ TinyInstaller/TinyInstaller/UserControls/HelpPanel.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace TinyInstaller.UserControls
 {
@@ -7,9 +8,29 @@ namespace TinyInstaller.UserControls
     /// </summary>
     public partial class HelpPanel : UserControl
     {
+        // Using a DependencyProperty as the backing store for DescriptionText.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty DescriptionTextProperty =
+            DependencyProperty.Register("DescriptionText", typeof(string), typeof(HelpPanel), new PropertyMetadata(default));
+
+        // Using a DependencyProperty as the backing store for HeaderText.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty HeaderTextProperty =
+            DependencyProperty.Register("HeaderText", typeof(string), typeof(HelpPanel), new PropertyMetadata(default));
+
         public HelpPanel()
         {
             InitializeComponent();
+        }
+
+        public string DescriptionText
+        {
+            get { return (string)GetValue(DescriptionTextProperty); }
+            set { SetValue(DescriptionTextProperty, value); }
+        }
+
+        public string HeaderText
+        {
+            get { return (string)GetValue(HeaderTextProperty); }
+            set { SetValue(HeaderTextProperty, value); }
         }
     }
 }
